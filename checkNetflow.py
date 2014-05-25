e30fa164974bde9d16fcd1f115c0bfc620536f15
young <young.mwy@gmail.com>
2014-05-25 15:47:08 +0800

    support sending email routinely

--- checkNetflow.py
+++ checkNetflow.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python
 #encoding=utf-8
 
-import sys, string, time
+import sys, string, time, calendar
 import mailInfo, logInfo
 from getInfo import getInfohitsun
 from sendMail import sendMail
-
-# get cuurent date and time
-localTime = time.localtime(time.time())
-currentTime = time.strftime('%Y-%m-%d %H:%M:%S', localTime)
 
 # results required from hitsun.hit.edu.cn
 dictInfo = getInfohitsun()
@@ -21,32 +17,73 @@ deposit = string.atof(deposit[0])
 
 # mail content
 content = '''Hi, all,\n
-The netflow has reached %.2f GB until %s in this month.
-Our lab's net balance is %.2f and the deposit is %.2f.\n
+The netflow of IP address %s has reached [%.2f GB] until now in this month.
+Our lab's net balance is [%.2f] and the deposit is [%.2f].\n
+----------
+This is sended by checkNetflow daemon automaticlally, please don't reply.\n
 Thanks,
-checkNetflow daemon''' %(netflow, currentTime, balance, deposit)
+checkNetflow''' %(logInfo.username, netflow, balance, deposit)
 
+# internal functions
+def getCurrentTime() :
+	localTime = time.localtime(time.time())
+	Year = string.atoi( time.strftime('%Y', localTime) )
+	Month = string.atoi( time.strftime('%m', localTime) )
+	Day = string.atoi( time.strftime('%d', localTime) )
+	Weekday = calendar.weekday(Year, Month, Day)
+	Time = time.strftime('%H:%M:%S', localTime) 	
+	ret = [Year, Month, Day, Weekday, Time]
+	return ret
 
-if (len(sys.argv) == 2 and sys.argv[1] == 'a') :
+def printAccount() :
 	print "account infomation\n--------------------"
 	print "balance \t: %.2f\ndeposit \t: %.2f\nnetflow \t: %.4f GB" %(balance, deposit, netflow)
 	print "--------------------"
-elif (len(sys.argv) == 2 and sys.argv[1] == 'c') :
+
+def printContent() :
 	print "mail content\n--------------------\n" + content + "\n----------"
-elif (len(sys.argv) == 2 and sys.argv[1] == 'd') :
+
+def printDebug() :
 	print "debug information\n--------------------"
 	print "host URL \t: %s\npost URL \t: %s" %(logInfo.hostURL, logInfo.postURL)
 	print "IP \t\t: %s\nPassword \t: %s" %(logInfo.username, logInfo.password)
 	print "Mailsender \t: %s\nMailpasswd \t: %s" %(mailInfo.sender, mailInfo.password)
 	print "Tolist \t\t: %s\nCclist \t\t: %s" %(mailInfo.tolist, mailInfo.cclist)
 	print "Subject \t: %s\nAttachments \t: %s" %(mailInfo.subject, mailInfo.filespath)
+	[Year, Month, Day, Weekday, Time] = getCurrentTime()
+	print "Date \t\t: %d-%d-%d\nWeekday \t: %d\nTime \t\t: %s" %(Year, Month, Day, Weekday, Time)
 	print "--------------------"
-elif (len(sys.argv) == 2 and sys.argv[1] == 'm') :
+	
+def sendDirectMail() :
 	if sendMail(mailInfo.subject, content) :
 		print "send mail successfully."
 	else :
 		print "send mail failed."
-else :
-	print "usage unavaliable. (usage : ./checkNetflow.py [i|c|d|m])"
-	print "a - Account information | c - mail Content | d - Debug information | m - send Mail" 
 
+def sendRoutineMail() :
+	while True :
+		time.sleep(1)
+		[Year, Month, Day, Weekday, Time] = getCurrentTime()
+		if (Weekday == 0 and Time == '00:00:00') :
+			sendDirectMail()
+
+def printUsage() :
+	print "usage unavaliable. (usage : ./checkNetflow.py [i|c|d|m])"
+	print "a - Account information\nc - mail Content\nd - Debug information\nm - send direct Mail\nr - send Routine mail" 
+
+
+# main control
+if (len(sys.argv) == 2 and sys.argv[1] == 'a') :
+	printAccount()
+elif (len(sys.argv) == 2 and sys.argv[1] == 'c') :
+	printContent()
+elif (len(sys.argv) == 2 and sys.argv[1] == 'd') :
+	printDebug()
+elif (len(sys.argv) == 2 and sys.argv[1] == 'm') :
+	sendDirectMail()
+elif (len(sys.argv) == 2 and sys.argv[1] == 'r') :
+	sendRoutineMail()
+else :
+	printUsage()
+
+	

7a87236854568aa6acde62641145de3f31f895c2
young <young.mwy@gmail.com>
2014-05-26 09:57:49 +0800

    fix a severe bug, get realtime netflow

--- checkNetflow.py
+++ checkNetflow.py
@@ -6,23 +6,6 @@ import mailInfo, logInfo
 from getInfo import getInfohitsun
 from sendMail import sendMail
 
-# results required from hitsun.hit.edu.cn
-dictInfo = getInfohitsun()
-balance = dictInfo['balance']
-balance = string.atof(balance[0])
-netflow = dictInfo['netflow']
-netflow = string.atof(netflow[0].replace(',','')) / 1000 / 1000
-deposit = dictInfo['deposit']
-deposit = string.atof(deposit[0])
-
-# mail content
-content = '''Hi, all,\n
-The netflow of IP address %s has reached [%.2f GB] until now in this month.
-Our lab's net balance is [%.2f] and the deposit is [%.2f].\n
-----------
-This is sended by checkNetflow daemon automaticlally, please don't reply.\n
-Thanks,
-checkNetflow''' %(logInfo.username, netflow, balance, deposit)
 
 # internal functions
 def getCurrentTime() :
@@ -35,13 +18,38 @@ def getCurrentTime() :
 	ret = [Year, Month, Day, Weekday, Time]
 	return ret
 
+def getProcessedInfo() :
+	dictInfo = getInfohitsun()
+	balance = dictInfo['balance']
+	balance = string.atof(balance[0])
+	netflow = dictInfo['netflow']
+	netflow = string.atof(netflow[0].replace(',','')) / 1000 / 1000
+	deposit = dictInfo['deposit']
+	deposit = string.atof(deposit[0])
+	ret = [balance, netflow, deposit]
+	return ret
+
+def getContent() :
+	[balance, netflow, deposit] = getProcessedInfo()
+	[Year, Month, Day, Weekday, Time] = getCurrentTime()
+	content = '''Hi, all,\n
+The netflow of IP address %s has reached [%.2f GB] until %d-%d-%d %s in this month.
+Our lab's net balance is [%.2f] and the deposit is [%.2f].\n
+----------
+This is sended by checkNetflow daemon automaticlally, please don't reply.\n
+Thanks,
+checkNetflow''' %(logInfo.username, netflow, Year, Month, Day, Time, balance, deposit)
+	return content
+
 def printAccount() :
+	[balance, netflow, deposit] = getProcessedInfo()
 	print "account infomation\n--------------------"
 	print "balance \t: %.2f\ndeposit \t: %.2f\nnetflow \t: %.4f GB" %(balance, deposit, netflow)
 	print "--------------------"
 
 def printContent() :
-	print "mail content\n--------------------\n" + content + "\n----------"
+	content = getContent()
+	print "mail content\n--------------------\n" + content + "\n--------------------"
 
 def printDebug() :
 	print "debug information\n--------------------"
@@ -51,10 +59,12 @@ def printDebug() :
 	print "Tolist \t\t: %s\nCclist \t\t: %s" %(mailInfo.tolist, mailInfo.cclist)
 	print "Subject \t: %s\nAttachments \t: %s" %(mailInfo.subject, mailInfo.filespath)
 	[Year, Month, Day, Weekday, Time] = getCurrentTime()
-	print "Date \t\t: %d-%d-%d\nWeekday \t: %d\nTime \t\t: %s" %(Year, Month, Day, Weekday, Time)
+	weekdayName = ['Mon', 'Tue', 'Wes', 'Thr', 'Fri', 'Sat', 'Sun']
+	print "Date \t\t: %d-%d-%d\nWeekday \t: %s\nTime \t\t: %s" %(Year, Month, Day, weekdayName[Weekday], Time)
 	print "--------------------"
 	
 def sendDirectMail() :
+	content = getContent()
 	if sendMail(mailInfo.subject, content) :
 		print "send mail successfully."
 	else :
@@ -64,12 +74,15 @@ def sendRoutineMail() :
 	while True :
 		time.sleep(1)
 		[Year, Month, Day, Weekday, Time] = getCurrentTime()
+		# each Monday 00:00:00
 		if (Weekday == 0 and Time == '00:00:00') :
 			sendDirectMail()
 
 def printUsage() :
-	print "usage unavaliable. (usage : ./checkNetflow.py [i|c|d|m])"
+	print "usage unavaliable. (usage : ./checkNetflow.py [i|c|d|m|r])"
+	print "--------------------"
 	print "a - Account information\nc - mail Content\nd - Debug information\nm - send direct Mail\nr - send Routine mail" 
+	print "--------------------"
 
 
 # main control
